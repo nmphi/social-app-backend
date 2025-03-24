99869277ab500e043de50e44961da7f415a56b0d
nmphi <minhphi03@gmail.com>
2025-03-24 19:52:24 +0700

    last-chance

--- connect.js
+++ connect.js
@@ -1,21 +1,11 @@
-import mysql from 'mysql';
+import mysql from "mysql";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-export const db = mysql.createPool({
-  connectionLimit: 10, // Giới hạn số kết nối đồng thời
+export const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
-  port: 3306
-});
-
-// Kiểm tra kết nối
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("Database connection failed:", err.message);
-  } else {
-    console.log("Connected to MySQL database");
-    connection.release(); // Giải phóng kết nối
-  }
+  database: process.env.DB_NAME,
 });
